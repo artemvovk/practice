e3f37cc12c82244a0987a551ae2fd28a95e8ae92
Artem Vovk <artemavovk@gmail.com>
2019-01-15 18:12:35 -0700

    maze finder

--- python/test/test_leetcode.py
+++ python/test/test_leetcode.py
@@ -22,11 +22,11 @@ from leetcode import problems
 def test_is_number(test_input, expected):
     assert problems.is_number(test_input) == expected
 
-def create_tree(val):
-    root = problems.TreeNode(val)
+def create_tree(height):
+    root = problems.TreeNode(height)
     current_node = root
     leftovers = []
-    trunk = random.randint(2, 10)
+    trunk = height
     for count in range(trunk):
         if count%2:
             current_node.left = problems.TreeNode(random.randint(0, 30))
@@ -44,11 +44,28 @@ def create_tree(val):
     print("Total made: {}".format(trunk + len(leftovers) + 2*len(leftovers)))
     return root
 
+def create_coins(amount):
+    if amount > 1 and amount%2:
+        amount -= 1
+    coins = [0] * amount
+    for index in range(amount):
+        coins[index] = random.randint(1, 100)
+    return coins
+
+def create_maze(length):
+    maze = [['O' for i in range(length)] for i in range(length)]
+    # pylint: disable=consider-using-enumerate
+    for y in range(len(maze)):
+        for x in range(len(maze[y])):
+            if not random.randint(55, 100)%7:
+                maze[y][x] = 'X'
+    return maze
+
 @pytest.mark.parametrize("test_input,expected", [
     (10, 0),
-    (20, 0)
+    (20, 0),
+    (300, 0)
 ])
-# pylint: disable=redefined-outer-name
 def test_min_camera_cover(test_input, expected):
     root = create_tree(test_input)
     cams, node = problems.min_cameras(0, root)
@@ -56,3 +73,25 @@ def test_min_camera_cover(test_input, expected):
         cams += 1
     print("{} cameras needed with root {}".format(cams, node))
     assert cams > expected
+
+@pytest.mark.parametrize("test_input,expected", [
+    (10, 0),
+    (20, 0)
+])
+def test_x_shapes(test_input, expected):
+    maze = create_maze(test_input)
+    print("\n")
+    for row in maze:
+        print(row)
+    shapes = problems.count_x_shapes(maze)
+    print(shapes)
+    assert shapes > expected
+
+@pytest.mark.parametrize("test_input,expected", [
+    (10, 0),
+    (20, 0)
+])
+def test_coin_game(test_input, expected):
+    coins = create_coins(test_input)
+    print(coins)
+    assert problems.coin_game(coins) > expected
